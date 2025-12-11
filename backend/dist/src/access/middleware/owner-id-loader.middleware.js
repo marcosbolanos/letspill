b8c0ab9d2b9dce6635b4fbc8a7390d3ce55c0534
marcos <marcosqbv@gmail.com>
2025-12-11 18:31:18 +0100

    chore: removed dist folder

--- backend/dist/src/access/middleware/owner-id-loader.middleware.js
+++
@@ -1,5 +0,0 @@
-import { createMiddleware } from 'hono/factory';
-export const ownerIdLoaderMiddleware = createMiddleware(async (c, next) => {
-    //TODO: implement this and pass it to routes that require access
-    // We currently use helper functions which are cluttering the code
-});
--- backend/dist/src/authorization/middleware/require-access.middleware.js
+++
@@ -1,2 +0,0 @@
-export {};
-// TODO: implement this middleware to make authorization cleaner
