a40cb819f87ac6510a18915d9b39d71788857121
marcos <marcosqbv@gmail.com>
2025-12-17 10:55:12 +0100

    fix: moved .github folder to root

--- frontend/utils/auth-client.ts
+++ frontend/utils/auth-client.ts
@@ -3,12 +3,8 @@ import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 
-// Use 10.0.2.2 for Android emulator, localhost for iOS simulator
 const getBaseURL = () => {
-  if (Platform.OS === "android") {
-    return "http://10.122.155.125:3000";
-  };
-  return "http://localhost:3000";
+  return process.env.EXPO_PUBLIC_API_URL;
 };
 
 export const authClient = createAuthClient({
